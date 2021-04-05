c9a08f522fa40d4c4f914c6f99fe280295d24677
vincent841 <41100208+vincent841@users.noreply.github.com>
2021-04-05 15:45:58 +0900

    remove build & sample files

--- sample/sample.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const dsrlib = require("@things-factory/node-dsr")
-
-var nodeDsr = new dsrlib.NodeDsr("192.168.137.50");
-console.log("openConnection: ", nodeDsr.openConnection());
-console.log("closeConnection: ", nodeDsr.closeConnection());
